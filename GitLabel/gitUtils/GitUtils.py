d6784ac95d26efe311ac230a29d046daa8d4f427
Luana <lms7@cin.ufpe.br>
2015-12-28 16:29:46 -0300

    Add git status method

--- GitLabel/gitUtils/GitUtils.py
+++ GitLabel/gitUtils/GitUtils.py
@@ -10,20 +10,26 @@ def listBranches(path):
     currentWorkingPath = os.path.dirname(os.path.dirname(__file__))
     branchesFilepath = currentWorkingPath + '/files/branchesFilepath.txt'
     branchesFile = open(branchesFilepath, 'w+')
-    subprocess.call(["git", "branch", "-r"], stdout=branchesFile)
+    subprocess.call(["git", "branch", "-r"], stdout = branchesFile)
     b = open(branchesFilepath, 'r')
     branches = []
     for branchFullname in b:
         lines = branchFullname.split("/")
         print(lines[len(lines) - 1])
         branches.append(lines[len(lines) - 1])
-        
+
 def gitFetch():
-    subprocess.call(["git", "fetch"])
+    res = subprocess.call(["git", "fetch"])
+    print(res)
+
+def gitStatus():
+    res = subprocess.call(["git", "status"])
+    print(res)
 
 def checkoutBranch(branch):
-    subprocess.call(["git", "checkout", branch])
-    
+    res = subprocess.call(["git", "checkout", branch])
+    print('Checkout = ' + str(res))
+
 def listFolders(path):
     directories = []
     for (_, dirnames, _) in walk(path):
