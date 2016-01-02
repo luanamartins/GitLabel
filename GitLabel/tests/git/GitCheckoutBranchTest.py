aa291354670baa8c8a77abcf73f13cff40b01502
Luana <lms7@cin.ufpe.br>
2016-01-02 00:43:36 -0300

    Add unittests classes for git methods

---
+++ GitLabel/tests/git/GitCheckoutBranchTest.py
@@ -0,0 +1,10 @@
+import unittest
+
+class Test(unittest.TestCase):
+
+    def testGitCheckoutBranch(self):
+        pass
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ GitLabel/tests/git/GitFetchTest.py
@@ -0,0 +1,10 @@
+import unittest
+
+class Test(unittest.TestCase):
+
+    def testGitFetch(self):
+        pass
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
